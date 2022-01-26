5d65c8efc62db9d08e26a10eff57749d069eaff5
omarvegaabreu <omarvega@hotmail.com>
2022-01-26 10:23:12 -0500

    corrected url

--- src/context/auth/AuthState.js
+++ src/context/auth/AuthState.js
@@ -34,7 +34,7 @@ const AuthState = (props) => {
       setAuthToken(userToken);
     }
 
-    const res = await axios.get(`${CORS_URL}${API_URL}api/auth`);
+    const res = await axios.get(`${CORS_URL}/${API_URL}api/auth`);
 
     try {
       dispatch({
